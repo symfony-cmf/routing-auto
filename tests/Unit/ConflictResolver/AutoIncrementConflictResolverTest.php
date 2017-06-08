2ad610207f3d5d0162c7d84970abea447a091e98
Damien Flament <damien.flament@gmx.com>
2017-06-08 18:42:29 +0200

    Fixed `AutoIncrementConflictResolver` behavior
     - The URI is not modified if there is not any conflict
     - The conflicting routes are not searched within the collection containing the given context

--- tests/Unit/ConflictResolver/AutoIncrementConflictResolverTest.php
+++ tests/Unit/ConflictResolver/AutoIncrementConflictResolverTest.php
@@ -11,38 +11,129 @@
 
 namespace Symfony\Cmf\Component\RoutingAuto\Tests\Unit\ConflictResolver;
 
+use Prophecy\Argument;
+use Symfony\Cmf\Component\RoutingAuto\AdapterInterface;
 use Symfony\Cmf\Component\RoutingAuto\ConflictResolver\AutoIncrementConflictResolver;
+use Symfony\Cmf\Component\RoutingAuto\Model\AutoRouteInterface;
+use Symfony\Cmf\Component\RoutingAuto\UriContext;
+use Symfony\Cmf\Component\RoutingAuto\UriContextCollection;
 
 class AutoIncrementConflictResolverTest extends \PHPUnit_Framework_TestCase
 {
-    protected $adapter;
+    private $adapter;
+    private $uriContext;
+    private $collection;
+    private $conflictResolver;
 
     public function setUp()
     {
-        $this->adapter = $this->prophesize('Symfony\Cmf\Component\RoutingAuto\AdapterInterface');
+        $this->adapter = $this->prophesize(AdapterInterface::class);
+        $this->uriContext = $this->prophesize(UriContext::class);
+        $this->collection = $this->prophesize(UriContextCollection::class);
+
+        $this->uriContext->getCollection()->willReturn($this->collection);
 
         $this->conflictResolver = new AutoIncrementConflictResolver($this->adapter->reveal());
-        $this->uriContext = $this->prophesize('Symfony\Cmf\Component\RoutingAuto\UriContext');
     }
 
+    /**
+     * Provide the URIs present in the collection and the database.
+     *
+     * - uri (string): the conflicting URI
+     * - collection (array): the URIs of the routes contained in the collection
+     * - datadase (array): the URIs of the routes contained in the database
+     * - expectedUri (string): the expected URI after the conflict has been resolved
+     */
     public function provideResolveConflict()
     {
         return [
-            [
-                '/foobar/bar',
-                [
-                    '/foobar/bar-1',
-                ],
-                '/foobar/bar-2',
+            'a not conflicting URI' => [
+                'uri' => '/foobar/bar',
+                'collection' => [],
+                'database' => [],
+                'expectedUri' => '/foobar/bar',
             ],
-            [
-                '/foobar/bar',
-                [
+            'a conflict with one URI in the collection' => [
+                'uri' => '/foobar/bar',
+                'collection' => [
+                    '/foobar/bar',
+                ],
+                'database' => [],
+                'expectedUri' => '/foobar/bar-1',
+            ],
+            'a conflict with one URI in the database' => [
+                'uri' => '/foobar/bar',
+                'collection' => [],
+                'database' => [
+                    '/foobar/bar',
+                ],
+                'expectedUri' => '/foobar/bar-1',
+            ],
+            'a conflict with three consecutive URI in the collection' => [
+                'uri' => '/foobar/bar',
+                'collection' => [
+                    '/foobar/bar',
+                    '/foobar/bar-1',
+                    '/foobar/bar-2',
+                ],
+                'database' => [],
+                'expectedUri' => '/foobar/bar-3',
+            ],
+            'a conflict with three consecutive URI in the database' => [
+                'uri' => '/foobar/bar',
+                'collection' => [],
+                'database' => [
+                    '/foobar/bar',
+                    '/foobar/bar-1',
+                    '/foobar/bar-2',
+                ],
+                'expectedUri' => '/foobar/bar-3',
+            ],
+            'a conflict with four consecutive URI in both the collection and the database' => [
+                'uri' => '/foobar/bar',
+                'collection' => [
+                    '/foobar/bar',
+                    '/foobar/bar-2',
+                ],
+                'database' => [
+                    '/foobar/bar-1',
+                    '/foobar/bar-3',
+                ],
+                'expectedUri' => '/foobar/bar-4',
+            ],
+            'a conflict with three not consecutive URIs in the collection' => [
+                'uri' => '/foobar/bar',
+                'collection' => [
+                    '/foobar/bar',
                     '/foobar/bar-1',
                     '/foobar/bar-2',
                     '/foobar/bar-4',
                 ],
-                '/foobar/bar-3',
+                'database' => [],
+                'expectedUri' => '/foobar/bar-3',
+            ],
+            'a conflict with three not consecutive URIs in then database' => [
+                'uri' => '/foobar/bar',
+                'collection' => [],
+                'database' => [
+                    '/foobar/bar',
+                    '/foobar/bar-1',
+                    '/foobar/bar-2',
+                    '/foobar/bar-4',
+                ],
+                'expectedUri' => '/foobar/bar-3',
+            ],
+            'a conflict with four not consecutive URI in both the collection and the database' => [
+                'uri' => '/foobar/bar',
+                'collection' => [
+                    '/foobar/bar',
+                    '/foobar/bar-2',
+                ],
+                'database' => [
+                    '/foobar/bar-1',
+                    '/foobar/bar-4',
+                ],
+                'expectedUri' => '/foobar/bar-3',
             ],
         ];
     }
@@ -50,16 +141,52 @@ class AutoIncrementConflictResolverTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider provideResolveConflict
      */
-    public function testResolveConflict($uri, $existingRoutes, $expectedResult)
-    {
+    public function testResolveConflict(
+        $uri,
+        array $collectionUris,
+        array $databaseUris,
+        $expectedUri
+    ) {
         $this->uriContext->getUri()->willReturn($uri);
 
-        foreach ($existingRoutes as $existingRoute) {
-            $this->adapter->findRouteForUri($existingRoute, $this->uriContext->reveal())->willReturn(new \stdClass());
-        }
-        $this->adapter->findRouteForUri($expectedResult, $this->uriContext->reveal())->willReturn(null);
+        $this->configureCollection($collectionUris);
+        $this->configureAdapter($databaseUris);
 
         $uri = $this->conflictResolver->resolveConflict($this->uriContext->reveal());
-        $this->assertEquals($expectedResult, $uri);
+
+        $this->assertEquals($expectedUri, $uri);
+    }
+
+    /**
+     * Configure the context collection.
+     *
+     * The context collection stub finds an auto route using the URI generated
+     * for a context it contains.
+     */
+    private function configureCollection(array $uris)
+    {
+        $this->collection->getAutoRouteByUri(Argument::type('string'))
+            ->willReturn(null);
+
+        foreach ($uris as $uri) {
+            $this->collection->getAutoRouteByUri($uri)
+                ->willReturn($this->prophesize(AutoRouteInterface::class)->reveal());
+        }
+    }
+
+    /**
+     * Configure the adapter.
+     *
+     * The adapter stub finds an auto route which matches for a given URI.
+     */
+    private function configureAdapter(array $uris)
+    {
+        $this->adapter->findRouteForUri(Argument::type('string'), $this->uriContext->reveal())
+            ->willReturn(null);
+
+        foreach ($uris as $uri) {
+            $this->adapter->findRouteForUri($uri, $this->uriContext->reveal())
+                ->willReturn($this->prophesize(AutoRouteInterface::class)->reveal());
+        }
     }
 }
